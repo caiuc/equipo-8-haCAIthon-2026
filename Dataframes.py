d62573b4fc01548a2bbda936a737009a6f8a0469
PKwyo <tmenke@estudiante.uc.cl>
2026-08-14 14:03:56 -0400

    Update Dataframes.py

--- Dataframes.py
+++ Dataframes.py
@@ -1,14 +1,10 @@
 import pandas as pd
 
-archivos = [
-    "calendar.txt",
-    "calendar_dates.txt"
-]
-
-dataframes = {}
-
-for archivo in archivos:
-    nombre = archivo.replace(".txt", "")
-    dataframes[nombre] = pd.read_csv(archivo)
-
-print(dataframes["calendar"])
+calendar = pd.read_csv("calendar.txt",parse_dates=["start_date", "end_date"],date_format="%Y%m%d")
+calendar_dates = pd.read_csv("calendar_dates.txt", parse_dates=["date"],date_format="%Y%m%d")
+frequencies = pd.read_csv("frequencies.txt")
+levels = pd.read_csv("levels.txt")
+pathways = pd.read_csv("pathways.txt")
+routes = pd.read_csv("routes.txt")
+stops = pd.read_csv("stops.txt")
+trips = pd.read_csv("trips.txt")
